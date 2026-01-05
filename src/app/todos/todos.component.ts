54df62db7e7523944e5f68d0fee04daee991507f
julian-pc <julian.beutin@viacesi.fr>
2026-01-05 20:22:18 +0100

    :sparkles: Add authentification

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -31,6 +31,10 @@ export class TodosComponent implements OnInit {
     }
   }
 
+  deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+
   createTodo() {
     try {
       client.models.Todo.create({
